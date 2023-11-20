4744a06cf94703f74bea8689fee62ffccdfc6503
fevan <evan.ferron53@gmail.com>
2023-11-20 14:11:27 +0100

    :sparkles: | create intersectipon and way class

---
+++ traffic/Way.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace traffic
+{
+
+    public class Way
+    {
+        public bool canVehiculePass;
+        // create a var that contains all vehicule which are in the way
+
+        public Way();
+
+    }
+}
