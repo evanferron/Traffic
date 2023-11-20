a2b8dd5d42f690245a76fae606e5a821813bd6d5
fevan <evan.ferron53@gmail.com>
2023-11-20 14:18:05 +0100

    :pencil2: | edited commentary

--- traffic/Way.cs
+++ traffic/Way.cs
@@ -6,7 +6,8 @@ namespace traffic
     public class Way
     {
         public bool canVehiculePass;
-        // create a var that contains all vehicule which are in the way
+        // create a var that contains all vehicules which are in the way
+        // create a var that contains all pedestrials which are in the way
 
         public Way();
 
