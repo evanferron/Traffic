dd9384c2d28b772e91e044633ef03cb3caad574b
fevan <evan.ferron53@gmail.com>
2023-11-21 12:44:30 +0100

    :pencil2: | edit change light func

--- traffic/TrafficLight.cs
+++ traffic/TrafficLight.cs
@@ -25,25 +25,42 @@ namespace traffic
             return true;
         };
 
-        public async void changeLight()
+        public void changeLight()
         {
-            await while (true)
+            switch (light)
             {
-                switch (light)
-                {
-                    case TrafficLightEnum.Orange:
-                        value = new Timer(async _ => await light = TrafficLightEnum.Red, null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
-                        break;
-                    case TrafficLightEnum.Red:
-                        value = new Timer(async _ => await light = TrafficLightEnum.Green, null, TimeSpan.Zero, TimeSpan.FromSeconds(4));
-                        break;
-                    default:
-                        value = new Timer(async _ => await light = TrafficLightEnum.Orange, null, TimeSpan.Zero, TimeSpan.FromSeconds(3));
-                        break;
+                case TrafficLightEnum.Orange:
+                    light = TrafficLightEnum.Red;
+                    break;
+                case TrafficLightEnum.Red:
+                    light = TrafficLightEnum.Green;
+                    break;
+                default:
+                    light = TrafficLightEnum.Orange;
+                    break;
 
-                }
             }
         }
+
+        // public async void changeLight()
+        // {
+        //     await while (true)
+        //     {
+        //         switch (light)
+        //         {
+        //             case TrafficLightEnum.Orange:
+        //                 light = new Timer(async _ => await light = TrafficLightEnum.Red, null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+        //                 break;
+        //             case TrafficLightEnum.Red:
+        //                 light = new Timer(async _ => await light = TrafficLightEnum.Green, null, TimeSpan.Zero, TimeSpan.FromSeconds(4));
+        //                 break;
+        //             default:
+        //                 light = new Timer(async _ => await light = TrafficLightEnum.Orange, null, TimeSpan.Zero, TimeSpan.FromSeconds(3));
+        //                 break;
+
+        //         }
+        //     }
+        // }
     }
 
     enum TrafficLightEnum
